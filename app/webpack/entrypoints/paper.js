a55930d4faa00dd2beaaf479d8d0752de8427a12
den1s0v <hello-lumen@ya.ru>
2021-12-31 00:42:37 +0300

    Дальнейший перенос настройки webpack

--- app/webpack/entrypoints/paper.js
+++ app/webpack/entrypoints/paper.js
@@ -1,3 +1,8 @@
-import "../javascripts/algorithms/paper/paper-full.min"
+
+// import "../javascripts/algorithms/paper/paper-core.min"
+import "paper/dist/paper-core.min"
+
 import "../javascripts/algorithms/paper/diagram"
 import "../javascripts/algorithms/paper/activity-vis"
+
+import "../javascripts/algorithms/show_diagram"
