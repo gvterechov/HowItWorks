50d9b5b28b523aa56a7702fd13bff7c03624bbfe
gvterechov <123freedom123@mail.ru>
2023-01-16 22:55:35 +0300

    Добавлен favicon

--- app/webpack/entrypoints/algorithms.js
+++ app/webpack/entrypoints/algorithms.js
@@ -3,3 +3,4 @@ import "../javascripts/algorithms/show_algorithms"
 import "../javascripts/algorithms/index_algorithms"
 
 import "../assets/images/diag-thumb-white.png"
+import "../assets/images/favicon.png"
