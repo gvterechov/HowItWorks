2be8d43624c3887702e4b812558be7d665621f5d
ZhovtDanil <dzhov123@mail.ru>
2025-07-03 20:14:53 +0000

    Удалены лишние роуты

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/edit'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
   get :publications, to: 'application#publications'
